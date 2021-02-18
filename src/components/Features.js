2283ffa2efcd8ffa6d0d38e22d36a858ca81eb8b
Reddemon Fox <him.sharma.98@gmail.com>
2021-02-18 21:34:48 +0530

    Supplier: sonft navigation in view-Edit modes

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,35 +2,49 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
-const FeatureGrid = ({ gridItems }) => (
-  <div className="columns is-multiline">
-    {gridItems.map((item) => (
-      <div key={item.text} className="column is-6">
-        <section className="section">
-          <div className="has-text-centered">
-            <div
-              style={{
-                width: '240px',
-                display: 'inline-block',
-              }}
-            >
-              <PreviewCompatibleImage imageInfo={item} />
+const FeatureGrid = ({gridItems}) => (
+    <div className="columns is-multiline">
+        {gridItems.map((item) => (
+            <div key={item.text} className="column is-6">
+                <section className="section menu-item" style={{padding: 0}}>
+                    <div className="has-text-centered image">
+                        <div
+                            style={{
+                                width: '240px',
+                                display: 'inline-block',
+                            }}
+                        >
+                            <PreviewCompatibleImage imageInfo={item}/>
+                        </div>
+                    </div>
+                    <div className="description-section">
+                        <div className="title">
+                            {
+                                item.name
+                            }
+                        </div>
+                        <p className="description">
+                            {
+                                item.description
+                            }
+                        </p>
+                        <p className="price">
+                            ₹ {item.price}
+                        </p>
+                    </div>
+                </section>
             </div>
-          </div>
-          <p>{item.text}</p>
-        </section>
-      </div>
-    ))}
-  </div>
+        ))}
+    </div>
 )
 
 FeatureGrid.propTypes = {
-  gridItems: PropTypes.arrayOf(
-    PropTypes.shape({
-      image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-      text: PropTypes.string,
-    })
-  ),
+    gridItems: PropTypes.arrayOf(
+        PropTypes.shape({
+            image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+            text: PropTypes.string,
+        })
+    ),
 }
 
 export default FeatureGrid
